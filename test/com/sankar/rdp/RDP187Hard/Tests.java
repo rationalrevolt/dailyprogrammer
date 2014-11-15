13fe4829a1f887c3eaeb9bee53c1049dbd211872
Sankaranarayanan Viswanathan <rationalrevolt@gmail.com>
2014-11-14 22:49:53 -0500

    Renamed tests

--- test/com/sankar/rdp/RDP187Hard/Tests.java
+++ test/com/sankar/rdp/RDP187Hard/Tests.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class RDP187HardTest {
+public class Tests {
 
     @Test
     public void test_simple_path() {
--- test/com/sankar/rdp/RDP188Easy/Tests.java
+++ test/com/sankar/rdp/RDP188Easy/Tests.java
@@ -19,14 +19,14 @@ import com.sankar.rdp.RDP188Easy.DateFixers.F5;
 import com.sankar.rdp.RDP188Easy.DateFixers.F6;
 
 @RunWith(Parameterized.class)
-public class RDP188EasyTest {
+public class Tests {
     
     private ProductionDateFixer fixer;
     
     private String input;
     private String expected;
     
-    public RDP188EasyTest(String input, String expected) {
+    public Tests(String input, String expected) {
         this.input = input;
         this.expected = expected;
     }
