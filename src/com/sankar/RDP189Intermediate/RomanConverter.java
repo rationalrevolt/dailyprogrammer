b912639b3701a8502a99c416ddf7f9d831f4ff2b
Sankaranarayanan Viswanathan <rationalrevolt@gmail.com>
2014-11-25 20:57:44 -0600

    Solution for Intermediate 189

---
+++ src/com/sankar/RDP189Intermediate/RomanConverter.java
@@ -0,0 +1,204 @@
+package com.sankar.RDP189Intermediate;
+
+
+public class RomanConverter {
+    
+    private enum Roman {
+        M(1000),
+        D(500),
+        C(100),
+        L(50),
+        X(10),
+        V(5),
+        I(1);
+        
+        private Roman(int value) {
+            this.value = value;
+        }
+        
+        public int value() {
+            return value;
+        }
+        
+        public int valueBySubtraction() {
+            return value() - romanToSubtract().value();
+        }
+        
+        public Roman romanToSubtract() {
+            switch(this) {
+                case V:
+                case X: return I;
+                case L:
+                case C: return X;
+                case D:
+                case M: return C;
+                
+                default: throw new AssertionError();
+            }
+        }
+        
+        public int maxRepeat() {
+            return this == M ? 4 : 3;
+        }
+        
+        public static Roman valueOf(char c) {
+            return valueOf(Character.toString(c));
+        }
+        
+        private int value;
+    }
+    
+    static int fromRoman(String inp) {
+        return new InternalFromRoman(inp).convert();
+    }
+    
+    static String toRoman(int inp) {
+        StringBuilder sb = new StringBuilder();
+        
+        if (inp >= 5000) {
+            int rest = inp % 1000;
+            int thousands = (inp - rest) / 1000;
+            
+            int thousandsTenth = thousands % 10;
+            
+            if (thousandsTenth > 0 && thousandsTenth < 4) {
+                rest += (1000 * thousandsTenth);
+                thousands -= thousandsTenth;
+            }
+            
+            else if (thousandsTenth > 5 && thousandsTenth < 9) {
+                int toShift = (thousandsTenth - 5);
+                rest += (1000 * toShift);
+                thousands -= toShift;
+            }
+            
+            sb.append('(').append(toRoman(thousands)).append(')').append(toRomanLessThan5000(rest));
+        }
+        
+        else
+            sb.append(toRomanLessThan5000(inp));
+        
+        return sb.toString();
+    }
+    
+    private static String toRomanLessThan5000(int inp) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(inp > 0) {
+            for(Roman r : Roman.values()) {
+                if (inp == r.value()) {
+                    sb.append(r.name());
+                    inp -= r.value();
+                    break;
+                }
+                
+                else if (r.value() > 1 && inp >= r.valueBySubtraction() && inp < r.value()) {
+                    sb.append(r.romanToSubtract().name()).append(r.name());
+                    inp -= r.valueBySubtraction();
+                    break;
+                }
+            
+                else {
+                    int repeat = 0;
+                    while(inp > r.value() && (++repeat <= r.maxRepeat())) {
+                        sb.append(r.name());
+                        inp -= r.value();
+                    }
+                    
+                    if (repeat > 0) break;
+                }
+            }
+        }
+        
+        return sb.toString();
+    }
+    
+    private static class InternalFromRoman {
+        
+        private String inp;
+        
+        private int currentIndex = 0;
+        
+        InternalFromRoman(String inp) {
+            this.inp = inp;
+        }
+        
+        int convert() {
+            return _convert(0);
+        }
+        
+        private int _convert(int nesting) {
+            int result = 0;
+            int multipliers = 0;
+            
+            Roman previous = null;
+            
+            int repeat = 0;
+            
+            while(currentIndex < inp.length()) {
+                
+                char c = inp.charAt(currentIndex);
+                
+                if (c == '(') {
+                    if (multipliers > 1) fail();
+                    
+                    currentIndex++;
+                    result += 1000 * _convert(nesting + 1);
+                    multipliers++;
+                    continue;
+                }
+                
+                else if (c == ')') {
+                    if (nesting == 0) fail();
+                    if (result == 0) fail();
+                    if (previous == Roman.I) fail();
+                    
+                    currentIndex++;
+                    return result;
+                }
+                
+                Roman current = Roman.valueOf(c);
+                
+                if (previous == null) {
+                    result += current.value();
+                    previous = current;
+                    repeat = 1;
+                    currentIndex++;
+                    continue;
+                }
+                
+                if (current == previous && repeat == current.maxRepeat()) 
+                    fail();
+                
+                if (current.value() == previous.value()) {
+                    result += current.value();
+                    repeat++;
+                }
+                
+                else if (current.value() < previous.value()) {
+                    result += current.value();
+                    repeat = 1;
+                }
+                
+                else if (repeat > 1) 
+                    fail();
+                
+                else {
+                    result += current.value();
+                    result -= 2 * previous.value();
+                    repeat = 1;
+                }
+                
+                previous = current;
+                currentIndex++;
+            }
+            
+            return result;
+        }
+    }
+    
+    private static void fail() {
+        throw new IllegalArgumentException("Invalid roman value");
+    }
+
+}
---
+++ test/com/sankar/RDP189Intermediate/RomanConverterTest.java
@@ -0,0 +1,71 @@
+package com.sankar.RDP189Intermediate;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RomanConverterTest {
+    
+    private String roman;
+    private int decimal;
+    
+    public RomanConverterTest(String roman, int decimal) {
+        this.roman = roman;
+        this.decimal = decimal;
+    }
+    
+    @Test public void
+    converts_roman_to_decimal_correctly() {
+        assertThat(String.format("Given %s", roman), decimal, is(RomanConverter.fromRoman(roman)));
+    }
+    
+    @Test public void
+    converts_decimal_to_roman_correctly() {
+        assertThat(String.format("Given %d", decimal), RomanConverter.toRoman(decimal), is(roman));
+    }
+    
+    @Parameters
+    public static Collection<Object[]> tests() {
+        return Arrays.asList(
+                test("IV", 4),
+                test("XXXIV", 34),
+                test("CCLXVII", 267),
+                test("DCCLXIV", 764),
+                test("CMLXXXVII", 987),
+                test("MCMLXXXIII", 1983),
+                test("MMXIV", 2014),
+                test("MMMM", 4000),
+                test("MMMMCMXCIX", 4999),
+                test("(V)", 5000),
+                test("(V)CDLXXVIII", 5478),
+                test("(V)M", 6000),
+                test("(IX)", 9000),
+                test("(X)M", 11000),
+                test("(X)MM", 12000),
+                test("(X)MMCCCXLV", 12345),
+                test("(CCCX)MMMMCLIX", 314159),
+                test("(DLXXV)MMMCCLXVII", 578267),
+                test("(MMMCCXV)CDLXVIII", 3215468),
+                test("(MMMMCCX)MMMMCDLXVIII", 4214468),
+                test("(MMMMCCXV)CDLXVIII", 4215468),
+                test("(MMMMCCXV)MMMCDLXVIII", 4218468),
+                test("(MMMMCCXIX)CDLXVIII", 4219468),
+                test("((XV)MDCCLXXV)MMCCXVI", 16777216),
+                test("((CCCX)MMMMCLIX)CCLXV", 314159265),
+                test("((MLXX)MMMDCCXL)MDCCCXXIV", 1073741824)
+        );
+    }
+    
+    private static Object[] test(String roman, int decimal) {
+        return new Object[] {roman, decimal};
+    }
+
+}
